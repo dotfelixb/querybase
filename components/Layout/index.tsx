54c2cbfa139f379723a536897feb2b4efba714a5
Kwaku Boamah <dotfelixb@gmail.com>
2021-06-02 22:52:55 +0000

    building searchbar

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -10,21 +10,25 @@ const Layout: React.FC<ILayoutProps> = ({ children }) => {
   return (
     <div className="w-full h-screen font-sans font-light bg-white">
       {/* menu bar */}
-      <div className="flex items-center fixed z10 w-full bg-indigo-500 inset-x-0 px-3 py-2 h-15">
-        <div className="flex w-1/2 text-white items-center">
-          <div className="pr-3">qbase</div>
+      <nav className="flex items-center fixed z10 w-full bg-indigo-500 inset-x-0 px-3 py-2 h-15">
+        <div className="flex w-1/2 items-center">
+          <div className="pr-3 ">
+            <span className="font-bold text-sm uppercase text-white">
+              QueryBase
+            </span>
+          </div>
           <div className="px-3">
-            <IconMenu icon={<PlusIcon className="w-6 h-6" />} />
+            <IconMenu icon={<PlusIcon className="w-6 h-6 text-white" />} />
           </div>
           <div className="px-3 w-full">
             <SearchBar />
           </div>
           <div className="w-1/3"></div>
         </div>
-        <div className="flex w-1/2 text-white justify-end">
-          <IconMenu icon={<AdjustmentsIcon className="w-6 h-6" />} />
+        <div className="flex w-1/2 justify-end">
+          <IconMenu icon={<AdjustmentsIcon className="w-6 h-6 text-white" />} />
         </div>
-      </div>
+      </nav>
       <div className="bg-indigo-50 w-full h-full">{children}</div>
     </div>
   );
--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,17 +1,49 @@
 import { SearchIcon } from "@heroicons/react/outline";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface ISearchBarProps {}
 
-const SearchBar: React.FC<ISearchBarProps> = () => {
+const SearchBar: React.FC<ISearchBarProps> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (evt) => {
+      if (evt.key === "Esc" || evt.key === "Escape") {
+        setIsOpen(false);
+      }
+    });
+    return () => {
+      document.removeEventListener("keydown", (evt) => {});
+    };
+  }, []);
+
   return (
-    <div className="flex w-full items-center bg-indigo-300 px-3 rounded">
-      <SearchIcon className="w-4 h-4 text-sm" />
-      <input
-        type="text"
-        placeholder="Search..."
-        className="bg-transparent w-full text-white font-semibold placeholder-white  text-sm px-1 py-2 outline-none ring-0 border-none focus:ring-0 focus:border-none"
-      />
+    <div className="relative flex flex-col w-full">
+      <div
+        onFocus={() => setIsOpen(true)}
+        className="relative z-10 flex w-full items-center bg-indigo-300 pl-3 pr-1 rounded"
+      >
+        <SearchIcon className="w-4 h-4 text-sm text-white" />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="bg-transparent w-full text-white font-semibold placeholder-white  text-sm px-1 py-2 outline-none ring-0 border-none focus:ring-0 focus:border-none"
+        />
+      </div>
+      {isOpen && (
+        <button
+          onClick={() => setIsOpen(false)}
+          className="fixed inset-0 bg-black opacity-5 w-full h-full cursor-default"
+          tabIndex={-1}
+        ></button>
+      )}
+      <div
+        className={`${
+          isOpen ? "" : "hidden"
+        } absolute w-full h-32 min-h-full bg-white mt-10 rounded shadow`}
+      >
+        {children}
+      </div>
     </div>
   );
 };
